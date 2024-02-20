0f06ffc971456dcc3527cb8e6196d1b0765f640b
Sean Pivin <seanpivin@gmail.com>
2024-02-20 15:59:47 +0200

    styling app changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,24 +16,22 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
+        <Row type="horizontal">
+          <Header type="h1">Zimmer Man</Header>
+          <Header type="h2">You take care the Guests</Header>
+          <Header type="h2">We take care the Rest</Header>
+          <div>
+            <Header type="h3">Check IN & Out</Header>
+            <Btn onClick={() => alert("Check In")}>Check-In</Btn> &nbsp;
+            <Btn onClick={() => alert("Check Out")}>Check-Out</Btn>
+          </div>
+        </Row>
         <Row type="vertical">
-          <Row type="horizontal">
-            <Header type="h1">Zimmer Man</Header>
-            <Header type="h2">You take care the Guests</Header>
-            <Header type="h2">We take care the Rest</Header>
-            <div>
-              <Header type="h3">Check IN & Out</Header>
-              <Btn onClick={() => alert("Check In")}>Check-In</Btn> &nbsp;
-              <Btn onClick={() => alert("Check Out")}>Check-Out</Btn>
-            </div>
-          </Row>
-          <Row type="vertical">
-            <Header type="h2">Form</Header>
-            <form>
-              <Input type="number" placeholder="Enter Room Number" />
-              <Input type="text" placeholder="Number Of Guests" />
-            </form>
-          </Row>
+          <Header type="h2">Form</Header>
+          <form>
+            <Input type="number" placeholder="Enter Room Number" />
+            <Input type="text" placeholder="Number Of Guests" />
+          </form>
         </Row>
       </StyledApp>
     </>
