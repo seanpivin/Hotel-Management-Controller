dc731e761ac05117b948354b90ef0abb876500b0
Sean Pivin <seanpivin@gmail.com>
2024-02-22 18:04:58 +0200

    Revert "creating the sideBar and headers"
    This reverts commit a61e5a7c199cf02f43001a53748241ab4ae33cde.

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 
-import Logo from "./Logo";
-import MainNav from "./MainNav";
-
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
@@ -12,12 +9,7 @@ const StyledSidebar = styled.aside`
 `;
 
 function Sidebar() {
-  return (
-    <StyledSidebar>
-      <Logo />
-      <MainNav />
-    </StyledSidebar>
-  );
+  return <StyledSidebar>Sidebar</StyledSidebar>;
 }
 
 export default Sidebar;
